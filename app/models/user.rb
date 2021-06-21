b41a03a4a28d6bf7af7e77f07d6b6247077d6bdb
Danilo Castro <danilogcastro@hotmail.com>
2021-06-21 12:22:37 -0300

    added associations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :tags
+  has_many :orders, through: :tags
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
