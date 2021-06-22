4ff225bbeb29d876b9b6d8b0aad3413823d17908
Bruno Talarico <bruno.talarico@gmail.com>
2021-06-22 14:23:31 +0100

    routes for tags
    Signed-off-by: Bruno Talarico <bruno.talarico@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/cartao', to: 'pages#cartao' 
-  get '/tag', to: 'pages#tag' 
+
+  #TAG related routes
+  get '/tag', to: 'pages#tag'
+  resources :tags, only: [ :index, :show, :new, :create, :edit, :update, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
