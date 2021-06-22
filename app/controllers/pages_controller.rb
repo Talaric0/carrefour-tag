a6ef64ea89b059c39b3a75845f855dbf9990fda1
Bruno Talarico <bruno.talarico@gmail.com>
2021-06-22 15:05:17 +0100

    minor refactor
    Signed-off-by: Bruno Talarico <bruno.talarico@gmail.com>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,10 +13,6 @@ class PagesController < ApplicationController
 
   def tag
     @tags = Tag.where(user: current_user)
-    if @tags.empty?
-      redirect_to tag_info_path
-    else
-      redirect_to tags_path
-    end
+    @tags.empty? ? redirect_to tag_info_path : redirect_to tags_path
   end
 end
