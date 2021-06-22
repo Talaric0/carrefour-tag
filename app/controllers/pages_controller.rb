bcc4d7f520b1e6964de565b4a5c5743274fa6ae8
Bruno Talarico <bruno.talarico@gmail.com>
2021-06-22 15:06:12 +0100

    cancel refactoring due to dumb rails
    Signed-off-by: Bruno Talarico <bruno.talarico@gmail.com>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,6 +13,10 @@ class PagesController < ApplicationController
 
   def tag
     @tags = Tag.where(user: current_user)
-    @tags.empty? ? redirect_to tag_info_path : redirect_to tags_path
+    if @tags.empty?
+      redirect_to tag_info_path
+    else
+      redirect_to tags_path
+    end
   end
 end
