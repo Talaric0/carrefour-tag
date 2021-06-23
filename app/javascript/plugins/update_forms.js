0b17a003ff06c9e55593cd85fc28a057ccc4f0d5
Bruno Talarico <bruno.talarico@gmail.com>
2021-06-23 15:52:26 +0100

    refactor
    Signed-off-by: Bruno Talarico <bruno.talarico@gmail.com>

--- app/javascript/plugins/update_forms.js
+++ app/javascript/plugins/update_forms.js
@@ -7,9 +7,10 @@ export const updateTagModel = (id, tagModel) => {
     .then((data) => {
       tagModel.innerHTML = ""; // clear second dropdown
       data.forEach((model) => {
+        // create option elements to insert into the second dropdown
         const elem = `<option value="${model["name"].split(" ")[0]}">${
           model["name"]
-        }</option>`; // create option elements to insert into the second dropdown, bank_name is the chosen label method in the form
+        }</option>`;
         tagModel.insertAdjacentHTML("beforeend", elem); // insert options into the dropdown
       });
     });
