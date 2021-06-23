1129cce4846557c648cb786949a09c327f39f71b
Bruno Talarico <bruno.talarico@gmail.com>
2021-06-23 14:08:27 +0100

    added fetch on new action from tags, makers array as collection on maker field
    Signed-off-by: Bruno Talarico <bruno.talarico@gmail.com>

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class TagsController < ApplicationController
   before_action :set_tag, only: %i[ show edit update destroy ]
 
@@ -13,6 +15,14 @@ class TagsController < ApplicationController
   # GET /tags/new
   def new
     @tag = Tag.new
+
+    # fetch makers array
+    makers_url = 'https://fipeapi.appspot.com/api/1/carros/marcas.json'
+    serialized = URI.open(makers_url).read
+    @makers =  []
+    JSON.parse(serialized).each do |maker|
+      @makers << maker['fipe_name']
+    end
   end
 
   # GET /tags/1/edit
