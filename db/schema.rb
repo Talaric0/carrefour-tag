90d76051fa8636962c6d5a2cbc964c4929b5dca2
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-06-30 14:54:33 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_153946) do
+ActiveRecord::Schema.define(version: 2021_06_28_155226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 2021_06_29_153946) do
     t.integer "year"
     t.boolean "canceled"
     t.string "unlock_code", default: ""
-    t.text "ocr"
     t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
